9e1f5ed23a89dc8903307c5e6cb139932ff55fe3
Noxygen <nox@noxygen.eu>
2018-05-06 19:42:13 +0200

    Introduced MemoryWaveProvider & MemorySampleProvider

---
+++ NAudio/Wave/SampleProviders/MemorySampleProvider.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace NAudio.Wave
+{
+    /// <summary>
+    /// A read-only <see cref="ISampleProvider"/> whose backing store is memory.
+    /// </summary>
+    public class MemorySampleProvider : ISampleProvider
+    {
+        private readonly object sourceLock = new object();
+        private readonly float[] source;
+
+        #region Properties
+
+        private int position;
+
+        /// <inheritdoc />
+        public WaveFormat WaveFormat { get; }
+
+        /// <summary>
+        /// Gets the length of the data source in samples.
+        /// </summary>
+        public int Length => source.Length;
+
+        /// <summary>
+        /// Gets or sets the current position within the data source.
+        /// </summary>
+        public int Position
+        {
+            get => position;
+            set { lock (sourceLock) position = value; }
+        }
+
+        #endregion
+
+        /// <summary>Initializes a new instance of the <see cref="MemorySampleProvider" /> class based on the specified float array.</summary>
+        /// <param name="data">The array of floats from which to create the current provider.</param>
+        /// <param name="format">The WaveFormat of this sample provider.
+        /// It needs to specify a format of <see cref="WaveFormatEncoding.IeeeFloat"/> type.</param>
+        public MemorySampleProvider(float[] data, WaveFormat format)
+        {
+            WaveFormat = format;
+            source = data;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="MemorySampleProvider" /> class based on the specified float array.</summary>
+        /// <param name="data">The array of floats from which to create the current provider. </param>
+        /// <param name="sampleRate">Sample Rate</param>
+        /// <param name="numChannels">Number of channels</param>
+        public MemorySampleProvider(float[] data, int sampleRate, int numChannels)
+        {
+            WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, numChannels);
+            source = data;
+        }
+
+        /// <inheritdoc />
+        public int Read(float[] buffer, int offset, int count)
+        {
+            lock (sourceLock)
+            {
+                var len = Math.Min(count, Length - Position);
+
+                Buffer.BlockCopy(source, Position * 4, buffer, offset * 4, len * 4);
+
+                Position += len;
+                return len;
+            }
+        }
+    }
+}
---
+++ NAudio/Wave/WaveProviders/MemoryWaveProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+// ReSharper disable once CheckNamespace
+namespace NAudio.Wave
+{
+    /// <summary>
+    /// Creates a writable <see cref="IWaveProvider"/> based on <see cref="MemoryStream" />.
+    /// </summary>
+    public class MemoryWaveProvider : MemoryStream, IWaveProvider
+    {
+        /// <inheritdoc />
+        public WaveFormat WaveFormat { get; }
+
+        /// <summary>Initializes a new instance of the <see cref="MemoryWaveProvider" /> class with an expandable capacity initialized to zero.</summary>
+        /// <param name="waveFormat">The WaveFormat of this wave provider.</param>
+        public MemoryWaveProvider(WaveFormat waveFormat)
+        {
+            WaveFormat = waveFormat;
+        }
+
+        /// <summary>Initializes a new non-resizable instance of the <see cref="MemoryWaveProvider" /> class based on the specified byte array.</summary>
+        /// <param name="buffer">The array of unsigned bytes from which to create the current stream.</param>
+        /// <param name="waveFormat">The WaveFormat of this wave provider.</param>
+        public MemoryWaveProvider(WaveFormat waveFormat, byte[] buffer) : base(buffer)
+        {
+            WaveFormat = waveFormat;
+        }
+
+        /// <summary>Initializes a new non-resizable instance of the <see cref="MemoryWaveProvider" /> class based on the specified byte array.</summary>
+        /// <param name="buffer">The array of unsigned bytes from which to create the current stream.</param>
+        /// <param name="writable">The setting of the <see cref="MemoryStream.CanWrite" /> property, which determines whether the stream supports writing.</param>
+        /// <param name="waveFormat">The WaveFormat of this wave provider.</param>
+        public MemoryWaveProvider(WaveFormat waveFormat, byte[] buffer, bool writable) : base(buffer, writable)
+        {
+            WaveFormat = waveFormat;
+        }
+    }
+}
